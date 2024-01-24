33f5da99c2088cde854389744cea3b17f25f224c
NachitheProtogen <nils.christian2006@gmail.com>
2024-01-24 22:09:02 +0100

    init

---
+++ Searchbar.js
@@ -0,0 +1,9 @@
+(function search() {
+  let cx = 'a63a4b30bc38149af';
+  let gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
