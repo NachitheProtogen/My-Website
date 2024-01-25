be1d62dcc63cfb2f18a9984e78c8988815229706
NachitheProtogen <nils.christian2006@gmail.com>
2024-01-25 16:13:11 +0100

    Version 0.2.1 Just some fixes and changes to the links also added Nachis character to the home website

---
+++ fooderscript.js
@@ -0,0 +1,39 @@
+//The script that will help me solve all my problems cause I can't bare to eddit 3 html files to add the same thing
+
+document.addEventListener("DOMContentLoaded", function () {
+    let footer = document.createElement("footer");
+    footer.className = "footer";
+  
+    let footerLinksDiv = document.createElement("div");
+    footerLinksDiv.className = "footer-links";
+  
+    let links = [
+      { id: "Youtube", href: "https://www.youtube.com/channel/UC_ey0FNtdHaFJCQjSqN-wLQ", text: "Youtube" },
+      { id: "Mail", href: "https://mail.google.com/mail/u/0/#inbox", text: "Gmail" },
+      { id: "Github", href: "https://github.com/NachitheProtogen", text: "Github" },
+      { id: "Reddit", href: "https://www.reddit.com/user/Affectionate-Pain229", text: "Reddit" },
+      { id: "Twitter", href: "https://twitter.com/Musicprotogen", text: "Twitter" },
+      { id: "Discord", href: "https://discord.com/users/324313743796207641", text: "Discord" },
+    ];
+  
+    links.forEach(function (linkInfo) {
+      let link = document.createElement("p");
+      link.className = "link";
+      link.id = linkInfo.id;
+      link.innerHTML = '<a href="' + linkInfo.href + '" target="_blank">' + linkInfo.text + '</a>';
+      footerLinksDiv.appendChild(link);
+    });
+  
+    let versionDiv = document.createElement("div");
+    versionDiv.className = "div";
+  
+    let version = document.createElement("p");
+    version.className = "version";
+    version.textContent = "Version 0.2.1";
+    versionDiv.appendChild(version);
+  
+    footer.appendChild(footerLinksDiv);
+    footer.appendChild(versionDiv);
+  
+    document.body.appendChild(footer);
+  });
