844ddb8a8d79c0f75cd8c3b77641c904130610b5
NachitheProtogen <nils.christian200@gmail.com>
2024-01-26 10:45:13 +0100

    v0.2.3 Icons for the footer links and now the site has a favorite icon :P

--- fooderscript.js
+++ fooderscript.js
@@ -1,39 +1,39 @@
 //The script that will help me solve all my problems cause I can't bare to eddit 3 html files to add the same thing
 
 document.addEventListener("DOMContentLoaded", function () {
-    let footer = document.createElement("footer");
-    footer.className = "footer";
-  
-    let footerLinksDiv = document.createElement("div");
-    footerLinksDiv.className = "footer-links";
-  
-    let links = [
-      { id: "Youtube", href: "https://www.youtube.com/channel/UC_ey0FNtdHaFJCQjSqN-wLQ", text: "Youtube" },
-      { id: "Github", href: "https://github.com/NachitheProtogen", text: "Github" },
-      { id: "Reddit", href: "https://www.reddit.com/user/Affectionate-Pain229", text: "Reddit" },
-      { id: "Twitter", href: "https://twitter.com/Musicprotogen", text: "Twitter" },
-      { id: "Discord", href: "https://discord.com/users/324313743796207641", text: "Discord" },
-      { id: "Steam", href: "https://steamcommunity.com/id/Nanachi_OwO/", text: "Steam"},
-    ];
-  
-    links.forEach(function (linkInfo) {
-      let link = document.createElement("p");
-      link.className = "link";
-      link.id = linkInfo.id;
-      link.innerHTML = '<a href="' + linkInfo.href + '" target="_blank">' + linkInfo.text + '</a>';
-      footerLinksDiv.appendChild(link);
-    });
-  
-    let versionDiv = document.createElement("div");
-    versionDiv.className = "div";
-  
-    let version = document.createElement("p");
-    version.className = "version";
-    version.textContent = "Version 0.2.2";
-    versionDiv.appendChild(version);
-  
-    footer.appendChild(footerLinksDiv);
-    footer.appendChild(versionDiv);
-  
-    document.body.appendChild(footer);
+  let footer = document.createElement("footer");
+  footer.className = "footer";
+
+  let footerLinksDiv = document.createElement("div");
+  footerLinksDiv.className = "footer-links";
+
+  let links = [
+    { id: "Youtube", icon: "fab fa-youtube", href: "https://www.youtube.com/channel/UC_ey0FNtdHaFJCQjSqN-wLQ", text: "Youtube" },
+    { id: "Github", icon: "fab fa-github", href: "https://github.com/NachitheProtogen", text: "Github" },
+    { id: "Reddit", icon: "fab fa-reddit", href: "https://www.reddit.com/user/Affectionate-Pain229", text: "Reddit" },
+    { id: "Twitter", icon: "fab fa-twitter", href: "https://twitter.com/Musicprotogen", text: "Twitter" },
+    { id: "Discord", icon: "fab fa-discord", href: "https://discord.com/users/324313743796207641", text: "Discord" },
+    { id: "Steam", icon: "fab fa-steam", href: "https://steamcommunity.com/id/Nanachi_OwO/", text: "Steam"},
+  ];
+
+  links.forEach(function (linkInfo) {
+    let link = document.createElement("p");
+    link.className = "link";
+    link.id = linkInfo.id;
+    link.innerHTML = '<a href="' + linkInfo.href + '" target="_blank"><i class="' + linkInfo.icon + '"></i> ' + linkInfo.text + '</a>';
+    footerLinksDiv.appendChild(link);
   });
+
+  let versionDiv = document.createElement("div");
+  versionDiv.className = "div";
+
+  let version = document.createElement("p");
+  version.className = "version";
+  version.textContent = "Version 0.2.3";
+  versionDiv.appendChild(version);
+
+  footer.appendChild(footerLinksDiv);
+  footer.appendChild(versionDiv);
+
+  document.body.appendChild(footer);
+});
