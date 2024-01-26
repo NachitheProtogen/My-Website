080e7c0afaca4a122d470a0647eb73f943b98511
NachitheProtogen <nils.christian200@gmail.com>
2024-01-26 22:00:01 +0100

    v0.2.4 set up block will work on it soon

--- fooderscript.js
+++ fooderscript.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   let version = document.createElement("p");
   version.className = "version";
-  version.textContent = "Version 0.2.3";
+  version.textContent = "Version 0.2.4";
   versionDiv.appendChild(version);
 
   footer.appendChild(footerLinksDiv);
