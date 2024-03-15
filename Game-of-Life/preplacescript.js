57422dd593255a816fce8895ea671bbded63f475
NachitheProtogen <nils.christian2006@gmail.com>
2024-03-15 17:30:49 +0100

    v1.2.0 Now got my own Programms section

---
+++ Game-of-Life/preplacescript.js
@@ -0,0 +1,107 @@
+//The script that will help me solve all my problems cause I can't bare to eddit 3 html files to add the same thing
+
+document.addEventListener("DOMContentLoaded", function() {
+    let header = document.createElement("header");
+    header.id = "myHeader";
+    header.className = "headerclass"
+    
+    let headerInfo = [
+      {id: "Headline", href: "index.html", text: "Nachi's website"},
+      {id: "Story", href: "story.html", text: "Story"},
+      {id: "Blog", href: "blog.html", text: "Blog"},
+      {id: "Artcorner", href: "Artcorner.html", text: "Artcorner"},
+      {id: "Nachi the Protogen", href: "Nachi.html", text: "Nachi the Protogen"},
+    ];
+    
+    headerInfo.forEach(function(headerInfoP){
+      let Headerlink = document.createElement("p");
+      Headerlink.className = "headerlink";
+      Headerlink.id = headerInfoP.id;
+      Headerlink.innerHTML = '<a class="headerlink" href="' + headerInfoP.href + '">' + headerInfoP.text + '</a>';
+      header.appendChild(Headerlink);
+    })
+    
+    document.body.appendChild(header)
+    })
+    
+    document.addEventListener("DOMContentLoaded", function () {
+      let br = document.createElement("br");
+    
+      let footer = document.createElement("footer");
+      footer.className = "footer";
+      
+      let footerLinksDiv = document.createElement("div");
+      footerLinksDiv.className = "footer-links";
+    
+      let links = [
+        { id: "Youtube", icon: "fab fa-youtube", href: "https://www.youtube.com/channel/UC_ey0FNtdHaFJCQjSqN-wLQ", text: "Youtube" },
+        { id: "Github", icon: "fab fa-github", href: "https://github.com/NachitheProtogen", text: "Github" },
+        { id: "Reddit", icon: "fab fa-reddit", href: "https://www.reddit.com/user/Affectionate-Pain229", text: "Reddit" },
+        { id: "Twitter", icon: "fab fa-twitter", href: "https://twitter.com/Musicprotogen", text: "Twitter" },
+        { id: "Discord", icon: "fab fa-discord", href: "https://discord.com/users/324313743796207641", text: "Discord" },
+        { id: "Steam", icon: "fab fa-steam", href: "https://steamcommunity.com/id/Nanachi_OwO/", text: "Steam"},
+        { id: "Home", icon: "fas fa-home", href: "https://shadow-nachi.de", text: "Home" }, 
+      ];
+      
+      links.forEach(function (linkInfo) {
+        let link = document.createElement("p");
+        link.className = "link";
+        link.id = linkInfo.id;
+        link.innerHTML = '<a href="' + linkInfo.href + '" ><i class="' + linkInfo.icon + '"></i> ' + linkInfo.text + '</a>';
+        footerLinksDiv.appendChild(link);
+      });
+    
+      let versionDiv = document.createElement("div");
+      versionDiv.className = "div";
+      
+      let version = document.createElement("p");
+      version.className = "version";
+      version.textContent = "Version 1.2.0"; //Version number
+      versionDiv.appendChild(version);
+      
+      let clock = document.createElement("div");
+      clock.className = "clock";
+      clock.id = "clock";
+      
+      function updateClock() {
+        const localTime = new Date();
+        
+        const targetTimezone = "Europe/Berlin";
+        const options = { timeZone: targetTimezone };
+        const targetTime = localTime.toLocaleTimeString('en-US', options);
+        
+        clock.textContent = `Current time for me: ${targetTime}`;
+      };
+    
+      setInterval(updateClock, 1000);
+      
+      updateClock();
+      
+      
+      footer.appendChild(footerLinksDiv);
+      footer.appendChild(versionDiv);
+      footer.appendChild(clock);
+    
+      document.body.appendChild(br);
+      document.body.appendChild(footer);
+    });
+    
+    document.addEventListener("DOMContentLoaded", function () {
+      var header = document.getElementById("myHeader");
+      var prevScrollPos = window.scrollY;
+    
+      window.onscroll = function () {
+          var currentScrollPos = window.scrollY;
+    
+          if (prevScrollPos > currentScrollPos) {
+              // Scrolling up
+              header.style.top = "0";
+          } else {
+              // Scrolling down
+              header.style.top = "-60px"; 
+          }
+    
+          prevScrollPos = currentScrollPos;
+      };
+    });
+    
---
+++ Game-of-Life/script.js
@@ -0,0 +1,125 @@
+let cells = [];
+
+const rows = 32; // Man könnte das feld auch größer machen
+const cols = 32; // aber naja
+
+function countAliveNeighbors(cellInp) {
+
+  cellInp.neighbors = 0;
+
+  cells.forEach(cell => {
+    if(cell.x == cellInp.x -1 && cell.y == cellInp.y -1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x -1 && cell.y == cellInp.y ){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x -1 && cell.y == cellInp.y +1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x  && cell.y == cellInp.y -1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x  && cell.y == cellInp.y +1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x +1 && cell.y == cellInp.y -1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x +1 && cell.y == cellInp.y ){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}
+    if(cell.x == cellInp.x +1 && cell.y == cellInp.y +1){
+      
+      if(cell.style.backgroundColor == "black"){
+      cellInp.neighbors++;
+    }}    
+  });
+
+};
+
+function updateGrid(){
+  cells.forEach(cell => {
+    countAliveNeighbors(cell);
+  })
+  cells.forEach(cell => {
+    if(cell.style.backgroundColor == "black"){
+      // alive
+      if(cell.neighbors < 2){
+        // dies due to isolation
+        cell.style.backgroundColor = "white";
+      }else if(cell.neighbors > 3){
+        // dies due to overpopulation
+        cell.style.backgroundColor = "white";
+      }
+    }else{
+      if(cell.neighbors == 3){
+        // cell is being born in the next gen.
+        cell.style.backgroundColor = "black";
+      }
+    }
+  });
+};
+
+function createEmptyGrid(rows, cols) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+        const cell = document.createElement('button');
+        cell.className = "cell"
+        cell.style.backgroundColor = "white";
+        cell.x = i;
+        cell.y = j;
+        cell.neighbors = 0;
+        cell.addEventListener("click", () => {
+          if (cell.style.backgroundColor=="white") {
+            cell.style.backgroundColor = "black";
+          } else {
+            cell.style.backgroundColor = "white";
+          };
+          console.log(cell.x,cell.y);
+        }) 
+        canvas.appendChild(cell);
+        cells.push(cell);
+    }
+    canvas.appendChild(document.createElement('br'));
+};
+console.log(cells);
+};
+
+let timer;
+
+play.addEventListener("click", () => {
+  let swiftnes = parseFloat(swiftness.value);
+  if(swiftnes<100) {
+    swiftnes = 100;
+  };
+
+  timer = setInterval(() => {
+    updateGrid()
+  }, swiftnes);
+
+})
+
+pause.addEventListener("click", () => {
+clearInterval(timer);
+})
+
+reload.addEventListener("click", () => {
+  document.location.reload();
+})
+
+createEmptyGrid(rows, cols);
